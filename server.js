55ff75bbfd66996485c780a211c6161b2dd91d61
rosalib <rosalinbiswal009@gmail.com>
2024-01-23 10:56:15 +1100

    second commit

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 // load the key values pairs in the .env file as environment variables
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const app = express()
-const port = 8080
-const expressLayouts = require('express-ejs-layouts')
-const requestLogger = require('./middlewares/request_logger')
-const methodOverride = require('method-override')
-const db = require('./db') // implicit require index.js file
-const session = require('express-session')
-const setCurrentUser = require('./middlewares/set_current_user')
-const ensureLoggedIn = require('./middlewares/ensure_logged_in')
-const dishRouter = require('./routes/dish_router')
-const homeRouter = require('./routes/home_router')
-const sessionRouter = require('./routes/session_router')
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const expressLayouts = require("express-ejs-layouts");
+const requestLogger = require("./middlewares/request_logger");
+const methodOverride = require("method-override");
+const db = require("./db"); // implicit require index.js file
+const session = require("express-session");
+const setCurrentUser = require("./middlewares/set_current_user");
+const ensureLoggedIn = require("./middlewares/ensure_logged_in");
+const dishRouter = require("./routes/dish_router");
+const homeRouter = require("./routes/home_router");
+const sessionRouter = require("./routes/session_router");
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
 /*
                       http request
@@ -30,37 +30,38 @@ app.set('view engine', 'ejs')
 //--------+
 //        |
 //        V
-app.use(express.static('public'))
+app.use(express.static("public"));
 //        |
 // override with POST method with method in query string ?_method=DELETE
-app.use(methodOverride('_method'))
+app.use(methodOverride("_method"));
 //        |
-app.use(requestLogger)
+app.use(requestLogger);
 //        V
-app.use(session({
-  secret: process.env.SECRET,
-  resave: false,
-  saveUninitialized: true
-}))
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "mistyrose",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
-app.use(setCurrentUser)
+app.use(setCurrentUser);
 
-app.use(expressLayouts)
+app.use(expressLayouts);
 // parse the request body in the an object of key value pairs in req.body
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 //        |
 //        V
-app.use(sessionRouter)
-app.use(homeRouter)
-app.use(dishRouter)
+app.use(sessionRouter);
+app.use(homeRouter);
+app.use(dishRouter);
 //        |
 //        |
 // <------+ 404 route doesnt exist (show built in error page)
 
-
 app.listen(port, () => {
-  console.log(`listening on port ${port}`)
-})
+  console.log(`listening on port ${port}`);
+});
 
 // database
 // place to organise
@@ -74,7 +75,6 @@ app.listen(port, () => {
 
 // psql
 // psql commands \l \c \dt
-
 
 // authencation
 // password based login
